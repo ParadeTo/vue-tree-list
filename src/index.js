c1270b880b493a734c72f92631d0495931956fdd
Binbin Sun <binbin.sun@shopee.com>
2020-01-07 11:21:17 +0800

    feat: 🎸 export default with the install method

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@
 import VueTreeList from "./VueTreeList";
 import { Tree, TreeNode } from "./Tree";
 
+VueTreeList.install = Vue => {
+  Vue.component(VueTreeList.name, VueTreeList);
+};
+
+export default VueTreeList;
 export { Tree, TreeNode, VueTreeList };
